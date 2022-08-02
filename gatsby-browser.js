43b5eb7cec96dda1c2042bd203d1596bc952f76d
cduncan123 <80055846+cduncan123@users.noreply.github.com>
2022-08-03 01:46:12 +0200

    update-to-spet

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,14 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
+// all styles here
+import './src/assets/css/bootstrap.min.css'
+import 'animate.css'
+import './src/assets/css/boxicons.min.css'
+import './src/assets/css/flaticon.css'
+import "./src/assets/css/slick.css"
+import 'react-accessible-accordion/dist/fancy-example.css'
+import './src/assets/css/style.css'
+import './src/assets/css/responsive.css'
 
-// You can delete this file if you're not using it
+// If you want Multicolor comment out single
+// import './src/assets/css/colors/brink-pink-style.css'
+// import './src/assets/css/colors/pink-style.css'
+// import './src/assets/css/colors/purple-style.css'
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,32 @@
-import * as React from "react"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import React from "react"
+import Layout from "../components/_App/layout"
+import SEO from "../components/_App/seo"
+import { Link } from "gatsby";
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+    <Layout>
+        <SEO title="404: Not found" />
 
-export const Head = () => <Seo title="404: Not Found" />
+        <div className="error-area">
+            <div className="d-table">
+                <div className="d-table-cell">
+                    <div className="container">
+                        <div className="error-content">
+                            <div className="notfound-404">
+                                <h1>Oops!</h1>
+                            </div>
+                            <h3>404 - Page not found</h3>
+                            <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+
+                            <Link to="/it-startup" className="btn btn-primary">
+                                Go to Homepage
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Layout>
+)
 
 export default NotFoundPage
